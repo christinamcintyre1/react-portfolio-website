f1bd0793fd5456cea943239e01d6e45b99fcbd82
Christina McIntyre <christinamcintyre1@gmail.com>
2022-05-05 14:23:45 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+        <Footer />
+    </>
+)
+
+// function App() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//             <h1>Welcome to React</h1>
+//         </header>
+//       </div>
+//     );
+//   }
+  
+export default App
