90f8082d2493857b9007b4d97ea8b10515846fae
Christina McIntyre <christinamcintyre1@gmail.com>
2022-05-13 13:52:43 -0600

    changed icons on btns

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
         <div className="about__content p">
           <p>My passion for software development can be traced back to before I even knew what a curly brace was. As a child, I was always taking things apart to see how they worked. As an adult, I am still curious about how things work, but now I have books to read. During my 22 years at Walgreens, I constantly solved problems as a cashier, and then as a Store Manager, I was tasked with more complex issues. The key to effective problem solving is to make the solution simple without adding complexity.
 <br/>
-            Outside of learning software development, I spend my free time hiking with my dog and boyfriend and gardening. I am a huge Jennifer Garner fan, so if you know her, tell her you know her biggest fan! </p>
+            Outside of learning software development, I spend my free time hiking with my dog and boyfriend and  also gardening. I am a huge Jennifer Garner fan, so if you see her, tell her to find me here! </p>
         </div>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
       </div>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import CV from '../../assets/CV.pdf'
 import { MdOutlineMailOutline } from 'react-icons/md'
+import {AiOutlineCloudDownload} from 'react-icons/ai'
 import { GrDocumentDownload } from 'react-icons/gr'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} className='btn btn-primary'><GrDocumentDownload className='cta-icon'/> CV</a>
-        <a href='#contact' className='btn btn-primary'><MdOutlineMailOutline className='cta-icon'/>EMAIL</a>
+        <a href={CV} className='btn btn-primary'><AiOutlineCloudDownload className='cta-icon'/>
+        <h5>CV</h5></a>
+
+        <a href='#contact' className='btn btn-primary'><MdOutlineMailOutline className='cta-icon'/>
+        <h5>EMAIL</h5></a>
     </div>
   )
 }
